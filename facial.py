b8a28321a2baad9bf2cd1d3f6478c1b3012d9747
Gabriel Barros <gabrielbarros7608@gmail.com>
2024-11-16 16:20:48 -0300

    Main Channel

---
+++ facial.py
@@ -0,0 +1,58 @@
+import threading
+import os 
+import cv2
+from deepface import DeepFace 
+
+print("Caminha completo da imagem:", os.path.abspath("reference.jpg"))
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+counter = 0 
+
+face_match = False 
+
+
+reference_img = cv2.imread("reference.jpg")
+if reference_img is None:
+    print ("Erro na imagem")
+    exit()
+
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+        else: 
+            face_match = False
+    except ValueError: 
+        face_match = False 
+            
+
+
+while True: 
+    ret, frame = cap.read()
+
+    if ret: 
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1 
+
+        if face_match:
+            cv2.putText(frame, "OLA GABRIEL!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else: 
+            cv2.putText(frame, "SEM LEITURA!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3) 
+
+        cv2.imshow("video",frame)           
+
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break 
+
+cv2.destroyAllWindows() 
